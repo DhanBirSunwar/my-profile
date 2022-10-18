11e35bf8265be2ffb8b76d927f298d131a58bf42
Dhan Bir Sunwar <deanvr42@gmail.com>
2022-10-18 12:42:55 +0600

    initial commit

---
+++ components/_navigation.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius est excepturi hic illo itaque natus
+            necessitatibus pariatur reprehenderit ut voluptatum! Et fugiat in, magnam non officia placeat temporibus.
+            Explicabo, ut!
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi corporis deleniti distinctio
+            excepturi fugit id maxime molestiae odit! Cumque, eaque enim magni minus neque nisi odio odit veritatis.
+            Voluptate.
+        </div>
+    )
+}
